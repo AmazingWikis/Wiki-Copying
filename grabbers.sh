6c27dcb79835ca00209b967f84700b0db10ba501
Kevin Zhang <37385853+AmazingWikis@users.noreply.github.com>
2022-11-07 14:38:54 -0500

    Update grabbers.sh
    reordering user input

--- grabbers.sh
+++ grabbers.sh
@@ -6,15 +6,6 @@ cd /var/www/html/w/
 git clone https://gerrit.wikimedia.org/r/mediawiki/tools/grabbers.git 
 
 
-echo "Please provide custom namespaces and separate with a |. Do not add spacing"
-read customNamespaces
-
-namespaces = "0|1|2|3|4|5|6|7|8|9|10|11|14|15"
-
-if ![ -z "${customNamespaces}" ] {
-	namespaces = "$namespaces|$customNamespaces"
-}
-
 # build url
 echo "What is the subdomain?"
 read subdomain
@@ -46,12 +37,22 @@ echo "What is database name?"
 read dbName
 
 php /var/www/html/w/grabbers/grabbers/grabNamespaceInfo.php --dbpass="$dbPass" --dbuser="$dbUser" --url="$URL" 
-php /var/www/html/w/grabbers/grabbers/grabLogs.php --db="$dbName" --dbpass="$dbPass" --dbuser="$dbUser" --url="$URL"  --namespaces="$namespaces"
-php /var/www/html/w/grabbers/grabbers/grabText.php --db="$dbName" --dbpass="$dbPass" --dbuser="$dbUser"  --url="$URL"  --namespaces="$namespaces"
-php /var/www/html/w/grabbers/grabbers/grabFiles.php --db="$dbName" --dbpass="$dbPass" --dbuser="$dbUser" --url="$URL" 
-php /var/www/html/w/grabbers/grabbers/grabProtectedTitles.php --db="$dbName" --dbpass="$dbPass" --dbuser="$dbUser" ---url="$URL" 
-php /var/www/html/w/grabbers/grabbers/grabUserBlocks.php --db="$dbName" --dbpass="$dbPass" --dbuser="$dbUser" --url="$URL" 
-php /var/www/html/w/grabbers/grabbers/grabUserGroups.php --db="$dbName" --dbpass="$dbPass" --dbuser="$dbUser" --url="$URL" 
+
+echo "Please provide custom namespaces and separate with a |. Do not add spacing"
+read customNamespaces
+
+namespaces = "0|1|2|3|4|5|6|7|8|9|10|11|14|15"
+
+if ![ -z "${customNamespaces}" ] {
+	namespaces = "$namespaces|$customNamespaces"
+}
+
+php /var/www/html/w/grabbers/grabLogs.php --db="$dbName" --dbpass="$dbPass" --dbuser="$dbUser" --url="$URL"  --namespaces="$namespaces"
+php /var/www/html/w/grabbers/grabText.php --db="$dbName" --dbpass="$dbPass" --dbuser="$dbUser"  --url="$URL"  --namespaces="$namespaces"
+php /var/www/html/w/grabbers/grabFiles.php --db="$dbName" --dbpass="$dbPass" --dbuser="$dbUser" --url="$URL" 
+php /var/www/html/w/grabbers/grabProtectedTitles.php --db="$dbName" --dbpass="$dbPass" --dbuser="$dbUser" ---url="$URL" 
+php /var/www/html/w/grabbers/grabUserBlocks.php --db="$dbName" --dbpass="$dbPass" --dbuser="$dbUser" --url="$URL" 
+php /var/www/html/w/grabbers/grabUserGroups.php --db="$dbName" --dbpass="$dbPass" --dbuser="$dbUser" --url="$URL" 
 
 echo "remote wiki user?"
 read username
@@ -59,9 +60,9 @@ read username
 echo "remote wiki's botpassword?"
 read botpassword
 
-// botpassword required [bot, sysop]
-php /var/www/html/w/grabbers/grabbers/grabDeletedText.php --dbpass="$dbPass"  --dbuser="$dbUser" --url="$URL" --namespaces="$namespaces" --username="$username" --password="$botpassword"
-php /var/www/html/w/grabbers/grabbers/grabDeletedFiles.php --db="$dbName" --dbpass="$dbPass"  --dbuser="$dbUser" --url="$URL" --username="$username" --password="$botpassword"
+# botpassword required [bot, sysop]
+php /var/www/html/w/grabbers/grabDeletedText.php --dbpass="$dbPass"  --dbuser="$dbUser" --url="$URL" --namespaces="$namespaces" --username="$username" --password="$botpassword"
+php /var/www/html/w/grabbers/grabDeletedFiles.php --db="$dbName" --dbpass="$dbPass"  --dbuser="$dbUser" --url="$URL" --username="$username" --password="$botpassword"
 
 php /var/www/html/w/maintenance/checkBadRedirects.php --conf /var/www/html/w/LocalSettings.php
 
